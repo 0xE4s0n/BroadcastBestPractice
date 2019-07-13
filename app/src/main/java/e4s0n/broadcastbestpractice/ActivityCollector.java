2d78ac69fa3b70566a63a7713b2293ee92baca73
0xE4s0n <1181499949@qq.com>
2019-07-13 22:06:58 +0800

    Initial commit

---
+++ app/src/main/java/e4s0n/broadcastbestpractice/ActivityCollector.java
@@ -0,0 +1,25 @@
+package e4s0n.broadcastbestpractice;
+
+import android.app.*;
+import java.util.*;
+
+public class ActivityCollector {
+    public static List<Activity> activities = new ArrayList<>();
+    public static void addActivity(Activity activity) {
+        activities.add(activity);
+
+    }
+
+    public static void removeActivity(Activity activity) {
+        activities.remove(activity);
+    }
+
+    public static void finishAll() {
+        for (Activity activity : activities) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+        activities.clear();
+    }
+}
---
+++ app/src/main/java/e4s0n/broadcastbestpractice/MainActivity.java
@@ -0,0 +1,23 @@
+package e4s0n.broadcastbestpractice;
+
+import android.content.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
